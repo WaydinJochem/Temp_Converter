0763fa7acba97b9a6ec30b217c31eb7104629b5f
WaydinJochem <jochem7405@gmail.com>
2023-05-25 11:02:50 +0200

    Some work done

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 let celcius = document.querySelector("#degrees");
-let faren = document.querySelector(".faren");
 let outputValue = document.querySelector("#display");
-let outputReverse = document.querySelector(".diplay");
-
 let C, FT, F, CT;
 
 function calc1() {
-    let C = celcius.value;
-    let FT = (C * 9/5) + 32;
-    outputValue.value = FT;
-    
+    let C= celcius.value;
+    let CT = (C * 9/5) + 32;
+    outputValue.value = CT;
 }
-function calc2() {
-    let F = faren.value;
-    let CT = (F - 32) * (5/9);
-    outputReverse.value = CT;
-}
+ function calc2() {
+    let F = outputValue.value;
+    let FT = (F - 32) * (5/9);
+    celcius.value = FT;
+ }
+
